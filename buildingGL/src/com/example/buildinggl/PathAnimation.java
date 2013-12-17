4ff20693a1686a45457c4317f823675d74b4d1eb
Daniel Langerenken <daniel.langerenken@gmail.com>
2013-12-17 18:54:53 +1100

    animation improved

--- buildingGL/src/com/example/buildinggl/PathAnimation.java
+++ buildingGL/src/com/example/buildinggl/PathAnimation.java
@@ -3,49 +3,46 @@ package com.example.buildinggl;
 import java.util.Date;
 import java.util.List;
 
-import android.opengl.Matrix;
-
 import com.example.touch.Vector3D;
 
 public class PathAnimation {
 	public static final int INFINITY = -1;
-
 	private static final float epsilon = 0.1f;
+
 	private Vector3D currentPosition;
 	private int nextVector = 1;
 	private Vector3D nextPathElement;
 	private List<Vector3D> pointsOnPath;
 
-	private long startTime;
+	private long currentTime;
 	private float timeToFinish;
 	private float pathLength;
 
 	private boolean isFinished = false;
-	private float length;
-	private float height;
-	private float width;
-	private boolean shouldRotate;
+	private boolean isRunnning = false;
 
 	private int repeatCount = 1;
 	private int currentRepeatCount = 0;
 
 	public PathAnimation(List<Vector3D> pointsOnPath, float timeToFinish,
-			boolean shouldRotate, Float length, Float height, Float width,
-			int repeatCount) {
+			Float length, Float height, Float width, int repeatCount) {
 		this.pointsOnPath = pointsOnPath;
 		this.timeToFinish = timeToFinish;
-		this.shouldRotate = shouldRotate;
-		if (shouldRotate) {
-			this.length = length;
-			this.height = height;
-			this.width = width;
-		}
 		this.repeatCount = repeatCount;
 		this.nextVector = 0;
 		this.currentPosition = pointsOnPath.get(nextVector++);
 		this.nextPathElement = pointsOnPath.get(nextVector);
-		this.startTime = new Date().getTime();
+		this.currentTime = new Date().getTime();
 		calculatePathLength();
+	}
+
+	public void start() {
+		this.currentTime = new Date().getTime();
+		isRunnning = true;
+	}
+
+	public void pause() {
+		isRunnning = false;
 	}
 
 	public void reset() {
@@ -53,7 +50,7 @@ public class PathAnimation {
 		this.nextVector = 0;
 		this.currentPosition = pointsOnPath.get(nextVector++);
 		this.nextPathElement = pointsOnPath.get(nextVector);
-		this.startTime = new Date().getTime();
+		this.currentTime = new Date().getTime();
 	}
 
 	private void calculatePathLength() {
@@ -65,53 +62,53 @@ public class PathAnimation {
 		this.pathLength = distance;
 	}
 
-	public void animate(float[] modelMatrix) {
-		long timeNow = new Date().getTime();
-		long delta = timeNow - startTime;
+	public float[] animate() {
+		if (isRunnning) {
+			long timeNow = new Date().getTime();
+			long delta = timeNow - currentTime;
+			currentTime = timeNow;
 
-		if (!isFinished) {
-			double dX = nextPathElement.getX() - currentPosition.getX();
-			double dY = nextPathElement.getY() - currentPosition.getY();
-			double dZ = nextPathElement.getZ() - currentPosition.getZ();
+			if (!isFinished) {
+				double dX = nextPathElement.getX() - currentPosition.getX();
+				double dY = nextPathElement.getY() - currentPosition.getY();
+				double dZ = nextPathElement.getZ() - currentPosition.getZ();
 
-			float yaw = (float) Math.atan2(dZ, dX);
-			float pitch = (float) (Math.atan2(Math.sqrt(dZ * dZ + dX * dX), dY) + Math.PI);
+				float yaw = (float) Math.atan2(dZ, dX);
+				float pitch = (float) (Math.atan2(Math.sqrt(dZ * dZ + dX * dX),
+						dY) + Math.PI);
 
-			float pathToWalk = pathLength / timeToFinish * delta;
-			float distanceToNextPathElement = Vector3D.getDistance(
-					currentPosition, nextPathElement);
-			while (pathToWalk > distanceToNextPathElement - epsilon) {
-				currentPosition = nextPathElement;
-				pathToWalk -= distanceToNextPathElement;
-				nextVector++;
-				if (nextVector > pointsOnPath.size()) {
-					isFinished();
-					return;
-				}
-				nextPathElement = pointsOnPath.get(nextVector);
-				distanceToNextPathElement = Vector3D.getDistance(
+				float pathToWalk = pathLength / timeToFinish * delta;
+				float distanceToNextPathElement = Vector3D.getDistance(
 						currentPosition, nextPathElement);
-			}
+				while (pathToWalk > distanceToNextPathElement - epsilon) {
+					currentPosition = nextPathElement;
+					pathToWalk -= distanceToNextPathElement;
+					nextVector++;
+					if (nextVector > pointsOnPath.size()) {
+						isFinished();
+						return null;
+					}
+					nextPathElement = pointsOnPath.get(nextVector);
+					distanceToNextPathElement = Vector3D.getDistance(
+							currentPosition, nextPathElement);
+				}
 
-			float percentageToWalk = pathToWalk / distanceToNextPathElement;
-			float newX = (nextPathElement.getX() - currentPosition.getX())
-					* percentageToWalk;
-			float newY = (nextPathElement.getY() - currentPosition.getY())
-					* percentageToWalk;
-			float newZ = (nextPathElement.getZ() - currentPosition.getZ())
-					* percentageToWalk;
-			Vector3D difference = new Vector3D(newX, newY, newZ);
-			currentPosition.add(difference);
-			pathToWalk = 0;
+				float percentageToWalk = pathToWalk / distanceToNextPathElement;
+				float newX = (nextPathElement.getX() - currentPosition.getX())
+						* percentageToWalk;
+				float newY = (nextPathElement.getY() - currentPosition.getY())
+						* percentageToWalk;
+				float newZ = (nextPathElement.getZ() - currentPosition.getZ())
+						* percentageToWalk;
+				Vector3D difference = new Vector3D(newX, newY, newZ);
+				currentPosition.add(difference);
+				pathToWalk = 0;
 
-			Matrix.translateM(modelMatrix, 0, currentPosition.getX(),
-					currentPosition.getY(), currentPosition.getZ());
-
-			if (shouldRotate) {
-				GLHelper.rotateModel(modelMatrix, yaw, pitch, null, true,
-						width, length, height);
+				return new float[] { yaw, pitch, currentPosition.getX(),
+						currentPosition.getY(), currentPosition.getZ() };
 			}
 		}
+		return null;
 	}
 
 	private void isFinished() {
